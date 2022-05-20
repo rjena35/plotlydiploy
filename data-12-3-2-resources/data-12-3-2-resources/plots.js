61f29a21e204fe892f976ac14657a6bfdbca4242
Richard Jena <rjena35@yahoo.com>
2022-05-19 20:53:43 -0400

    .

---
+++ data-12-3-2-resources/data-12-3-2-resources/plots.js
@@ -0,0 +1,26 @@
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person => person.wfreq);
+    console.log(wfreq);
+});
+
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    console.log(wfreq);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    filteredWfreq = wfreq.filter(element => element !=
+null);
+    console.log(filteredWfreq);
+});
+
+console.log(Object.entries(researcher1));
